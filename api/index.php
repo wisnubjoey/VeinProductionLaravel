f87bdd8824abc948ad766b65f45b6af18f30b11f
wisnubjoey <wisnuhutama@outlook.com>
2024-12-04 14:25:47 +0800

    vercel bisa plis

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
+// Check if the request is for a static file
+if (preg_match('/\.(?:css|js|jpg|png|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false;
+}
 
 require __DIR__ . '/../public/index.php';
