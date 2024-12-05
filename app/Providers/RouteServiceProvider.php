c6cb7ff1536c2693632d216886b967217d5beaea
wisnubjoey <wisnuhutama@outlook.com>
2024-12-05 21:58:44 +0800

    setting RouteServiceProvider 1

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,8 +30,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+            ->prefix('') // Hapus prefix 'api'
+            ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
