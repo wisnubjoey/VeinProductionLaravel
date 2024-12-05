5812630af527cdd2d3a5cc27ffd5108ea0a4cb95
wisnubjoey <wisnuhutama@outlook.com>
2024-12-05 21:49:01 +0800

    api route not showing fix 1

--- api/index.php
+++ api/index.php
@@ -1,7 +1,15 @@
 <?php
-// Check if the request is for a static file
-if (preg_match('/\.(?:css|js|jpg|png|gif)$/', $_SERVER["REQUEST_URI"])) {
-    return false;
-}
 
-require __DIR__ . '/../public/index.php';
+// Load composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load Laravel
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// Run Laravel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
