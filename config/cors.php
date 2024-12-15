1d41eb8ffb0ca3551832362a350d9eac4be532cb
wisnubjoey <wisnuhutama@outlook.com>
2024-12-15 22:43:00 +0800

    connect online 2

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://vein-production-next-js.vercel.app/'],
+    'allowed_origins' => ['http://localhost:3000', 'https://vein-production-next-js.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
